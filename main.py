45ff95dad799bdd3e58444f4d3a23049411032ac
Sam Siam <66347142+sam1siam@users.noreply.github.com>
2023-01-20 16:51:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import praw 
+reddit = praw.Reddit( 
+    client_id="4H_n_mlP1g5dwYSeslvZjQ", 
+    client_secret="ykf51jrPB4hqzTMoAZvk5twD54gu1Q", 
+    password="swiss2512",  
+    user_agent="my user agent", 
+    username="ultracryptocurrency",
+    check_for_async=false
+)
+
+import random
+import time
+def karma():
+	try:
+		messages = ['Upvoted, upvote in return?', 'Great post, care to share the upvotes!']
+		for submission in reddit.subreddit("FreeKarma4U+FreeKarmaForYou").stream.submissions():
+			submission.upvote()
+			rand = random.randint(0, len(messages)-1)
+			print(submission.title)
+			submission.reply(messages[rand])
+			time.sleep(30)
+	except:
+		time.sleep(60)
+		karma()
+karma()
